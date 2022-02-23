955afc7a88cec8765971568379258eb13a7c8d31
Vince Falconi <vince@tattooed.dev>
2022-02-23 01:22:35 -0600

    try `exports` in `package.json`
    Signed-off-by: Vince Falconi <vince@tattooed.dev>

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const gulpfile = require('./src/gulpfile');
 const gulpConfig = require('./src/config.gulp.js');
 const scriptTasks = require('./src/gulp-tasks/scripts');
 const styleTasks = require('./src/gulp-tasks/styles');
 
 module.exports = {
-	gulpfile,
 	gulpConfig,
 	...scriptTasks,
 	...styleTasks,
