857d2902cd578385840be2a2681e5508efd2da62
Vince Falconi <vince@tattooed.dev>
2022-02-23 00:56:48 -0600

    Revert "Update postinstall.js"
    This reverts commit 0e2068d8ec02436d810f11810522c6c78e627b9f.
    
    # Conflicts:
    #	postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,11 +1,10 @@
-const { constants } = 'fs';
-const { copyFile } = 'fs/promises';
+import { constants } from 'fs';
+import { copyFile } from 'fs/promises';
 
-(async () => {
-  try {
-    await copyFile('./src/gulpfile.js', '../../gulpfile.js', constants.COPYFILE_EXCL);
-    console.log('gulpfile template was copied to project root');
-  } catch {
-    console.log('existing gulpfile detected, template not copied');
-  }
-})();
+// By using COPYFILE_EXCL, the operation will fail if destination.txt exists.
+try {
+  await copyFile('./src/gulpfile.js', '../../gulpfile.js', constants.COPYFILE_EXCL);
+  console.log('source.txt was copied to destination.txt');
+} catch {
+  console.log('The file could not be copied');
+}
