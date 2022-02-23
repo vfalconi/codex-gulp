ff0eff90c91d070a4837e2a8dd510efb5b502208
vince falconi <vince@tattooed.dev>
2022-02-23 00:49:59 -0600

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,11 @@
 const { constants } = 'fs';
 const { copyFile } = 'fs/promises';
 
-// By using COPYFILE_EXCL, the operation will fail if destination.txt exists.
-try {
-  await copyFile('./src/gulpfile.js', '../../gulpfile.js', constants.COPYFILE_EXCL);
-  console.log('gulpfile template was copied to project root');
-} catch {
-  console.log('existing gulpfile detected, template not copied');
-}
+(async () => {
+  try {
+    await copyFile('./src/gulpfile.js', '../../gulpfile.js', constants.COPYFILE_EXCL);
+    console.log('gulpfile template was copied to project root');
+  } catch {
+    console.log('existing gulpfile detected, template not copied');
+  }
+})();
