03bba7024cddf1577187ab21075bf7f8c76cdaad
Vince Falconi <vince@tattooed.dev>
2022-02-23 00:57:35 -0600

    fix `await`
    Signed-off-by: Vince Falconi <vince@tattooed.dev>

--- postinstall.js
+++ postinstall.js
@@ -3,7 +3,7 @@ const { copyFile } = 'fs/promises';
 
 // By using COPYFILE_EXCL, the operation will fail if destination.txt exists.
 try {
-  await copyFile('./src/gulpfile.js', '../../gulpfile.js', constants.COPYFILE_EXCL);
+  copyFile('./src/gulpfile.js', '../../gulpfile.js', constants.COPYFILE_EXCL);
   console.log('gulpfile template was copied to project root');
 } catch {
   console.log('existing gulpfile detected, template not copied');
